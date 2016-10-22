3b015491d04f47c3367d3a2170cff50bebad3012
GusWard <guss.warrd@googlemail.com>
2016-10-22 21:43:21 +0100

    Added initial travis.yml Removed old code from RoboUnitTests.

--- lib/src/test/java/uk/co/wardworks/megadroidsdk/RoboUnitTests.java
+++ lib/src/test/java/uk/co/wardworks/megadroidsdk/RoboUnitTests.java
@@ -5,24 +5,11 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import uk.co.wardworks.megadroidsdk.Activities.TestMegaActivity;
-
-import static uk.co.wardworks.megadroidsdk.Activities.MegaActivity.State.PRECEDING;
-
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 23)
 public class RoboUnitTests {
-
     @Test
-    public void testMegaActivity() throws Exception {
-
-        TestMegaActivity testMegaActivity = new TestMegaActivity();
-
-        assertEquals("TestMegaActivity", testMegaActivity.TAG);
-
-        assertEquals(PRECEDING, testMegaActivity.getState());
-
-        assertEquals(false, testMegaActivity.isVisible());
+    public void test() throws Exception {
     }
 
 }
